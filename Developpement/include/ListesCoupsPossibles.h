b91de12af664b6dcefc4b5131814c6670e9d7c8d
srasendrasoa <sandratra.rasendrasoa@insa-rouen.fr>
2015-12-02 09:32:35 +0100

    Ajout listes coups possibles dans le dvpt

---
+++ Developpement/include/ListesCoupsPossibles.h
@@ -0,0 +1,14 @@
+#ifndef __LISTES_COUPS_POSSIBLES__
+#define __LISTES_COUPS_POSSIBLES__
+
+/**
+ * \fn Coups listeCoupsPossibles(Plateau plateau, Couleur couleur)
+ * \brief Fonction qui retourne un ensemble de coups possibles
+ *
+ * \param Plateau plateau, le plateau 
+ * \param Couleur couleur, couleur du joueur courant
+ * \return Coups
+ */
+Coups listeCoupsPossibles(Plateau plateau, Couleur couleur); 
+
+#endif
---
+++ Developpement/src/ListesCoupsPossibles.c
@@ -0,0 +1,21 @@
+#include TAD_PLATEAU.h
+#include TAD_COUP.h
+#include TAD_COULEUR.h
+#include ListesCoupsPossibles_Prive.h
+#include ListesCoupsPossibles.h
+
+/* Partie publique  */
+
+Coups listeCoupsPossibles(Plateau plateau, Couleur couleur){
+
+};
+
+/* Partie privee  */
+
+int coupValide(Plateau plateau, Coup coup){
+
+};
+
+Plateau copierPlateau(Plateau plateau){
+
+};
