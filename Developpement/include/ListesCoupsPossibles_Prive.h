67e2eb4f5923eacf228dd51991da471657266d68
srasendrasoa <sandratra.rasendrasoa@insa-rouen.fr>
2015-12-02 13:11:50 +0100

    Corrections sur ListeCoupsPossibles privée et publique

--- Developpement/include/ListesCoupsPossibles_Prive.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LISTES_COUPS_POSSIBLES_PRIVE__
-#define __LISTES_COUPS_POSSIBLES_PRIVE__
-#include "ListesCoupsPossibles.h"
-
-int coupValide(Plateau plateau, Coup coup);
-Plateau copierPlateau(Plateau plateau);
-
-#endif
