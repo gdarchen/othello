7a63e85f3f510283eea99c3edd4249190328f112
Romain Judic <romain.judic@insa-rouen.fr>
2015-12-02 09:25:21 +0100

    Ajout des .h et .c de ObtenirCoupIA

---
+++ Developpement/include/ObtenirCoupIA.h
@@ -0,0 +1,29 @@
+/**
+ * \file ObtenirCoupIA.h
+ * \brief Implantation et signatures des fonctions publiques d'ObtenirCoupIA
+ * \author Groupe 1.5
+ * \version 1.0
+ * \date 02/12/2015
+ *
+ */
+
+#ifndef OBTENIR_COUP_IA
+#define OBTENIR_COUP_IA
+#include "TAD_Coup.h"
+#include "TAD_Plateau.h"
+#include "TAD_Couleur.h"
+
+
+ Coup obtenirCoupIA(plateau Plateau, couleur Couleur);
+
+
+ /**
+  * \fn Coup obtenirCoupIA(plateau Plateau, couleur Couleur)
+  * \brief Fonction permettant de récupérer le coup joué par l'IA
+  *
+  * \param Pateau plateau, le plateau du jeu en cours
+  * \param Couleur couleur, la couleur représentée par l'IA
+  * \return Coup
+  */
+
+  #endif
---
+++ Developpement/src/ObtenirCoupIA.c
@@ -0,0 +1,31 @@
+#include "ObtenirCoupIA.h"
+#include "ObtenirCoupIA_Prive.h"
+
+/* Partie publique */
+
+Coup obtenirCoupIA(plateau Plateau, couleur Couleur){
+
+};
+
+
+/* Partie privée */
+
+unsigned int profondeur(void){
+
+};
+
+int scoreDUnCoup(Plateau plateau, Couleur couleurRef, Couleur couleurCourante, unsigned int profondeurCourante){
+
+};
+
+int minMax(Plateau plateau, Couleur couleurRef, Couleur couleurCourante, int profondeurCourante){
+
+};
+
+int score(Plateau plateau, Couleur couleur){
+
+};
+
+int evaluerPlateau(Plateau plateau, Couleur couleur){
+
+};
