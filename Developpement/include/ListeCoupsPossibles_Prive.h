4ec00d9a5386369b9fee7c6291910162d2a1e883
srasendrasoa <sandratra.rasendrasoa@insa-rouen.fr>
2015-12-03 18:57:28 +0100

    Rajout de ListecoupsPossibles.h suite à une mauvaise manip

---
+++ Developpement/include/ListeCoupsPossibles_Prive.h
@@ -0,0 +1,10 @@
+#ifndef __LISTES_COUPS_POSSIBLES_PRIVE__
+#define __LISTES_COUPS_POSSIBLES_PRIVE__
+#include "ListesCoupsPossibles.h"
+#include "TAD_Coup.h"
+#include "TAD_Plateau.h"
+
+int coupValide(Plateau plateau, Coup coup);
+Plateau copierPlateau(Plateau plateau);
+
+#endif
