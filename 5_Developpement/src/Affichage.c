87e122619c16da72f5a67798790f9e9454b63c34
gdarchen <darchen.gautier@insa-rouen.fr>
2015-12-09 20:33:35 +0100

    Modification du RAPPORT, avec l'ajout des tableaux de réalisation dans les parties suivantes : Analyse, CP, et Développement.

--- 5_Developpement/src/Affichage.c
+++ 5_Developpement/src/Affichage.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "FaireUnePartie.h"
+#include "TAD_Couleur.h"
+#include "TAD_Pion.h"
+#include "TAD_Plateau.h"
+#include "TAD_Position.h"
 
 void afficherAide(){
-  printf("
-Aide du programme othello
-Les options possibles sont :
-  othello standard blanc|noir [profondeur>2]
-    permet de jouer contre l’ordinateur en lui donnant les blancs ou les noirs
-    par defaut la profondeur d’analyse est egale a 5
-
-  othello tournoi blanc|noir [profondeur>2]
-    permet de faire jouer le programme dans un mode ’tournoi’ en lui donnant les
-    blancs ou les noirs
-    par defaut la profondeur d’analyse est egale a 5");
+  printf("Aide du programme othello \n"
+  "Les options possibles sont : \n"
+    "othello standard blanc|noir [profondeur>2] \n"
+      "permet de jouer contre l’ordinateur en lui donnant les blancs ou les noirs\n"
+      "par defaut la profondeur d’analyse est egale a 5 \n \n"
+    "othello tournoi blanc|noir [profondeur>2] \n"
+      "permet de faire jouer le programme dans un mode ’tournoi’ en lui donnant les\n"
+      "blancs ou les noirs\n"
+      "par defaut la profondeur d’analyse est egale a 5");
 }
 
 void afficher (Plateau plateau){
+  int i,j;
+  Couleur couleurBlanc, couleurNoir;
+  couleurBlanc=CL_blanc();
+  couleurNoir=CL_noir();
   printf("   0   1   2   3   4   5   6   7 ");
-  printf("—————————————————————————————————");
-  Position* position;
-  for(int i=0;i<8;i++){
+  printf(" ————————————————————————————————");
+  Position position;
+  for(i=0;i<8;i++){
     printf("%d|", i);
-    for (int j=0;j<8;j++){
-      POS_fixerPosition(i,j,position)
-      if (!estCaseVide(plateau,position)){
-        
+    for (j=0;j<8;j++){
+      POS_fixerPosition(i,j,&position);
+      if (!PL_estCaseVide(plateau,position)){
+        if (CL_sontEgales((PI_obtenirCouleur(PL_obtenirPion(plateau,position))),couleurBlanc))
+          printf(" B ");
+        else 
+          printf(" N |");
       }
+      else
+        printf("   |");
     }
   }
+}
 
+int main(int argc, char const *argv[]){
+  Plateau plateau=PL_creerPlateau();
+  afficher(plateau);
+  return 0;
 }
