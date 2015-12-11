27740cc270421cac6c743491ede62a997cb55779
Claire Lovisa <claire.lovisa@insa-rouen.fr>
2015-12-11 20:28:48 +0100

    Ajout d'une vérification dans ObtenirCoupHumain.c

--- 5_Developpement/src/ObtenirCoupHumain.c
+++ 5_Developpement/src/ObtenirCoupHumain.c
@@ -4,15 +4,19 @@
 #include <stdio.h>
 
 Coup obtenirCoupHumain(Plateau plateau, Couleur couleur){
-  int x,y;
+  int i=-1,j=-1;
   Coup coup;
   Position position;
   Pion pion;
-  printf("Veuillez saisir un numéro de ligne (de 0 à 7) : \n");
-  scanf("%d \n",&x);
-  printf("Veuillez saisir un numéro de colonne (de 0 à 7) : \n");
-  scanf("%d \n",&y);
-  POS_fixerPosition(x,y,&position);
+    while((i!=0) && (i!=1) && (i!=2) && (i!=3) && (i!=4) && (i!=5) && (i!=6) && (i!=7)){
+      printf("Veuillez saisir un numéro de ligne (de 0 à 7) : \n");
+      scanf("%d",&i);
+    }
+    while((j!=0) && (j!=1) && (j!=2) && (j!=3) && (j!=4) && (j!=5) && (j!=6) && (j!=7)){
+      printf("Veuillez saisir un numéro de colonne (de 0 à 7) : \n");
+      scanf("%d",&j);
+    }
+  POS_fixerPosition(i,j,&position);
   pion=PI_creerPion(couleur);
   coup=CP_creerCoup(position,pion);
 
