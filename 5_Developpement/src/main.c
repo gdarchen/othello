15fbac2646499908a1da8be05a0fd356330e1f87
Claire Lovisa <claire.lovisa@insa-rouen.fr>
2015-12-11 19:15:14 +0100

    Correction de quelques erreurs du main

--- 5_Developpement/src/main.c
+++ 5_Developpement/src/main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "FaireUnePartie.h"
+#include "Affichage.h"
+#include "ObtenirCoupIA.h"
+#include "ObtenirCoupHumain.h"
 
 int main(int argc, char** argv){
 	Couleur vainqueur=CL_blanc();
 	int matchNul = 0;
-	FaireUnePartie(afficherPlateau,obtenirCoupHumain,obtenirCoupIA,&vainqueur,&matchNul);
-	
-	if(argc<>2){
-		afficherAide(); 
+	faireUnePartie(afficherPlateau,obtenirCoupHumain,obtenirCoupIA,&vainqueur,&matchNul);
+
+	if(argc!=2){
+		afficherAide();
 	}
 	else
 	{
 		if (argc==2 && argv[1]=="standard"){
-			FaireUnePartie(afficherPlateau,obtenirCoupHumain,obtenirCoupIA,&vainqueur,&matchNul);
+			faireUnePartie(afficherPlateau,obtenirCoupHumain,obtenirCoupIA,&vainqueur,&matchNul);
 		}
 		else
 		{
-			if (argc==2 && argv[1]="tournoi"){
-				FaireUnePartie(afficherPlateau,obtenirCoupIA,obtenirCoupIA,&vainqueur,&matchNul);
+			if (argc==2 && argv[1]=="tournoi"){
+				faireUnePartie(afficherPlateau,obtenirCoupIA,obtenirCoupIA,&vainqueur,&matchNul);
 			}
 		}
 	}
