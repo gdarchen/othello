9569b7aaa2e82c867bb9436a9b230b177663cdca
Claire Lovisa <claire.lovisa@insa-rouen.fr>
2015-12-15 17:21:53 +0100

    Petite amélioration de obtenirCoupHumain

--- 5_Developpement/src/ObtenirCoupHumain.c
+++ 5_Developpement/src/ObtenirCoupHumain.c
@@ -8,12 +8,18 @@ Coup obtenirCoupHumain(Plateau plateau, Couleur couleur){
   Coup coup;
   Position position;
   Pion pion;
+  if(CL_sontEgales(couleur,CL_blanc())){
+    printf("Joueur blanc, ");
+  }
+  else {
+    printf("Joueur noir, ");
+  }
     while((i!=0) && (i!=1) && (i!=2) && (i!=3) && (i!=4) && (i!=5) && (i!=6) && (i!=7)){
-      printf("Veuillez saisir un numéro de ligne (de 0 à 7) : \n");
+      printf("veuillez saisir un numéro de ligne (de 0 à 7) : \n");
       scanf("%d",&i);
     }
     while((j!=0) && (j!=1) && (j!=2) && (j!=3) && (j!=4) && (j!=5) && (j!=6) && (j!=7)){
-      printf("Veuillez saisir un numéro de colonne (de 0 à 7) : \n");
+      printf("veuillez saisir un numéro de colonne (de 0 à 7) : \n");
       scanf("%d",&j);
     }
   POS_fixerPosition(i,j,&position);
